47e102fce968806ff36f7cf1216d76cdf4701f21
Taisei-rgb <taisei.contact@gmail.com>
2023-05-06 10:01:40 +0900

    [WIP] クリックイベント

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,19 @@ import { Headline } from "@/components/Headline";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
 import { Navbar } from "@/components/Navbar";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+	const foo = 3;
+
+	const hundleClick = useCallback((e: any) => {
+		console.log(e.target);
+		e.preventDefault();
+		alert(foo);
+	}, []);
+
 	return (
 		<>
 			<Header>
@@ -18,6 +27,9 @@ export default function Home() {
 			<main className={styles.main}>
 				<Headline title="Index Page" />
 				<Main />
+				<a href="/about" onClick={hundleClick}>
+					ボタン
+				</a>
 				<Links />
 			</main>
 		</>
