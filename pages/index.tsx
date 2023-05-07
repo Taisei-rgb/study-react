0fe19daa85bbfcb6cd79f6ba9a3fdea1ad034013
Taisei-rgb <taisei.contact@gmail.com>
2023-05-07 18:57:06 +0900

    useStateを用いた文字列と真偽値について

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	const [count, setCount] = useState(1);
+	const [text, setText] = useState("");
+	const [isShow, setIsShow] = useState(true);
 
 	const hundleClick = useCallback(() => {
 		if (count < 10) {
 			setCount((count) => count + 1);
 		}
 	}, [count]);
+
+	const hundleDisplay = useCallback(() => {
+		setIsShow((isShow) => !isShow);
+	}, []);
+
+	const hundleChange = useCallback((e: any) => {
+		if (e.target.value.length >= 5) {
+			alert("5文字以下にしてください。");
+			return;
+		}
+		setText(e.target.value.trim());
+	}, []);
 
 	useEffect(() => {
 		// マウント時の処理
@@ -37,8 +51,12 @@ export default function Home() {
 			<main className={styles.main}>
 				<Headline title="Index Page" />
 				<Main />
-				<h1>{count}</h1>
+				{isShow ? <h1>{count}</h1> : <h1>カウントは〇〇です。</h1>}
 				<button onClick={hundleClick}>ボタン</button>
+				<button onClick={hundleDisplay}>
+					{isShow ? "カウントは〇〇です。ボタン" : "カウントボタン"}
+				</button>
+				<input type="text" value={text} onChange={hundleChange} />
 				<Links />
 			</main>
 		</>
